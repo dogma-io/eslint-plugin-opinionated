9cb839c7c4b1312a49cc3b22d740950ec121c6f5
Matthew Dahl <matt.dahl@carium.com>
2018-10-01 11:57:14 -0400

    Actually fix

--- scripts/fix-types.js
+++ scripts/fix-types.js
@@ -7,20 +7,20 @@ const INDEX_FLOW_PATH = join(__dirname, '..', 'lib', 'index.js.flow')
 
 const CONTENTS = readFileSync(INDEX_FLOW_PATH, 'utf8')
   .replace(
-    '{opinionated/jsx-spread-first:',
-    '{"opinionated/jsx-spread-first":',
+    'opinionated/jsx-spread-first:',
+    '"opinionated/jsx-spread-first":',
   )
   .replace(
-    '{jsx-spread-first:',
-    '{"jsx-spread-first":',
+    'jsx-spread-first:',
+    '"jsx-spread-first":',
   )
   .replace(
-    '{opinionated/react-component-flow-type-names:',
-    '{"opinionated/react-component-flow-type-names":',
+    'opinionated/react-component-flow-type-names:',
+    '"opinionated/react-component-flow-type-names":',
   )
   .replace(
-    '{react-component-flow-type-names:',
-    '{"react-component-flow-type-names":',
+    'react-component-flow-type-names:',
+    '"react-component-flow-type-names":',
   )
 
 writeFileSync(INDEX_FLOW_PATH, CONTENTS, 'utf8')
