95488016403285947e237653489b512f72b12f57
Matthew Dahl <matt.dahl@carium.com>
2018-10-01 11:05:25 -0400

    Add jsx-spread-first rule

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,11 +1,13 @@
 import index from '../index'
+import {rule as jsxSpreadFirst} from '../rules/jsx-spread-first'
 import {rule as reactComponentFlowTypeNames} from '../rules/react-component-flow-type-names'
 
-describe('eslint-plugin-react-compat', () => {
+describe('eslint-plugin-opinionated', () => {
   it('should define expected configs', () => {
     expect(index.configs).toEqual({
       recommended: {
         rules: {
+          'opinionated/jsx-spread-first': 2,
           'opinionated/react-component-flow-type-names': 2,
         },
       },
@@ -14,6 +16,7 @@ describe('eslint-plugin-react-compat', () => {
 
   it('should define expected rules', () => {
     expect(index.rules).toEqual({
+      'jsx-spread-first': jsxSpreadFirst,
       'react-component-flow-type-names': reactComponentFlowTypeNames,
     })
   })
